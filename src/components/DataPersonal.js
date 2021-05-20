7f79014f6b4b3cd1b9bf02aef75a3b496a006198
Ha young, Kim <jeremiah91@naver.com>
2021-05-20 10:51:35 +0900

    checkout depth_01 title

--- src/components/DataPersonal.js
+++ src/components/DataPersonal.js
@@ -2,16 +2,21 @@ import React from "react";
 import data_personal from "../db/data_personal.json";
 
 const DataPersonal = () => {
-  console.log(data_personal);
-  console.log(data_personal[0].menus);
-  console.log(data_personal[0].menus[0].app_name);
-  console.log(data_personal[0].menus[1].app_name);
-  console.log(data_personal[0].menus[2].app_name);
-  console.log(data_personal[0].menus[3].app_name);
-  console.log(data_personal[0].menus[4].app_name);
+  //   console.log(data_personal);
+  //   console.log(data_personal[0].menus);
+  //   console.log(data_personal[0].menus[0].app_name);
+  //   console.log(data_personal[0].menus[1].app_name);
+  //   console.log(data_personal[0].menus[2].app_name);
+  //   console.log(data_personal[0].menus[3].app_name);
+  //   console.log(data_personal[0].menus[4].app_name);
 
   console.log(data_personal[0].menus);
 
+  const depth_01 = data_personal[0].menus;
+  const depth_01_title = depth_01.map(function (element) {
+    return element.app_name;
+  });
+  console.log(depth_01_title);
   return <></>;
 };
 
