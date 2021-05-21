2ea4087db4d10a4930bc9b94ea3be45052b55199
Ha young, Kim <jeremiah91@naver.com>
2021-05-21 22:13:18 +0900

    menu tree of data_personal w/ dp_01, dp_02, dp_03

--- src/components/DataPersonal.js
+++ src/components/DataPersonal.js
@@ -1,36 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
 import data_personal from "../db/data_personal.json";
 
 const DataPersonal = () => {
   //   console.log(data_personal);
-  //   console.log(data_personal[0].menus);
   //   console.log(data_personal[0].menus[0].app_name);
   //   console.log(data_personal[0].menus[1].app_name);
   //   console.log(data_personal[0].menus[2].app_name);
   //   console.log(data_personal[0].menus[3].app_name);
   //   console.log(data_personal[0].menus[4].app_name);
 
-  const [depth_01__title, setDepth_01__title] = useState("");
+  const [KJ_SB] = data_personal;
+  console.log(KJ_SB.menus);
 
-  const title_01 = data_personal[0].menus.map(function (title) {
-    return <p>title.app_name</p>;
+  // 전체 메뉴 트리 :
+  const menu_tree = KJ_SB.menus.map((item, index) => {
+    // depth 01 메뉴 :
+    const depth_01__title = item.app_name;
+    const depth_01__url = item.app_url;
+    // depth 02 메뉴 :
+    const depth_02 = item.menus.map((item, index) => {
+      const depth_02__title = item.app_name;
+      const depth_02__url = item.app_url;
+      // depth 03 메뉴 :
+      const depth_03 = item.menus.map((item, index) => {
+        const depth_03__title = item.app_name;
+        const depth_03__url = item.app_url;
+        return (
+          <li key={index}>
+            <p>
+              {depth_03__title} <span>{depth_03__url}</span>
+            </p>
+          </li>
+        );
+      });
+
+      console.log(depth_02__title);
+      console.log(depth_03);
+      return (
+        <li key={index}>
+          <p>
+            {depth_02__title} <span>{depth_02__url}</span>
+          </p>
+          <ul>{depth_03}</ul>
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <ul>
+          <li key={index}>
+            <p>
+              {depth_01__title} <span>{depth_01__url}</span>{" "}
+            </p>
+            <ul>{depth_02}</ul>
+          </li>
+        </ul>
+      </div>
+    );
   });
 
-  const onChange__title = () => {
-    setDepth_01__title();
-  };
   return (
     <>
-      <div>
-        <form>
-          <input
-            type="text"
-            onChange={onChange__title}
-            placeholder="메뉴 리스트 변경"
-          />
-          {title_01}
-        </form>
-      </div>
+      <div>{menu_tree}</div>
     </>
   );
 };
