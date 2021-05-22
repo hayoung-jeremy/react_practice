bde0efe8f9e317e155e0fda9dde2fcd55eee7ff3
Ha young, Kim <jeremiah91@naver.com>
2021-05-23 00:20:37 +0900

    clear console.log

--- src/components/DataPersonal.js
+++ src/components/DataPersonal.js
@@ -3,11 +3,6 @@ import data_personal from "../db/data_personal.json";
 
 const DataPersonal = () => {
   //   console.log(data_personal);
-  //   console.log(data_personal[0].menus[0].app_name);
-  //   console.log(data_personal[0].menus[1].app_name);
-  //   console.log(data_personal[0].menus[2].app_name);
-  //   console.log(data_personal[0].menus[3].app_name);
-  //   console.log(data_personal[0].menus[4].app_name);
 
   const [KJ_SB] = data_personal;
   console.log(KJ_SB.menus);
