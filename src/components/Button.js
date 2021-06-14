ada2f854ae785e41323c370d5b9f5883caa3c96b
Ha young, Kim <jeremiah91@naver.com>
2021-06-14 23:13:41 +0900

    feat : Create Button Component  - install styled-components  - create default Button component and style  - render a Button link to my Github profile
    it will be updated (such as : size, color, outline, etc)

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-const Button = ({ children }) => {
-  return <button>{children}</button>;
+const CommonStyle = css`
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  padding: 8px 12px;
+  background-color: #fff;
+  font-size: 1rem;
+`;
+
+const StyledButton = styled.button`
+  ${CommonStyle}
+`;
+
+const Anchor = styled(StyledButton.withComponent("a"))`
+  ${CommonStyle}
+`;
+
+const Button = ({ children, onClick, isAnchor, isNewTap, href, ...rest }) => {
+  return isAnchor ? (
+    <Anchor href={href} target={isNewTap ? `_blank` : ""}>
+      {children}
+    </Anchor>
+  ) : (
+    <StyledButton onClick={onClick} {...rest}>
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,41 @@
+import Button from "../components/Button";
 import Main from "../pages/Main";
+
+// light mode, dark mode toggle button
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme === "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+const onClickToggleTheme = () => {
+  // toggle the .dark-theme class on each click
+  document.body.classList.toggle("dark-theme");
+
+  // default theme : light mode
+  let theme = "light";
+
+  // save the selected theme in localStorage
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+};
 
 function App() {
   return (
     <>
-      <header>Ha young Design</header>
+      <header>
+        Ha young Design
+        <Button onClick={onClickToggleTheme}>Switch Mode</Button>
+        <Button
+          isAnchor
+          isNewTap
+          href="https://github.com/hayoung-jeremy"
+          rel="noreferrer"
+        >
+          Github
+        </Button>
+      </header>
       <Main />
       <footer>©2021. Ha Young, Kim. all rights reserved.</footer>
     </>
