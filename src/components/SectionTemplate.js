0bbd0ff234969c9f3585b815b5f8d98692fb9379
Ha young, Kim <jeremiah91@naver.com>
2021-06-16 23:30:06 +0900

    feat : Add Section Component
    to clarify the sections, it has
     - box shadow,
     - 12px bottom margin
    
    **to be added :
     - inner
     - wrapper
     - container
     etc.

---
+++ src/components/SectionTemplate.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  width: 100%;
+  padding: 20px;
+  background-color: #262626;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.04);
+  box-sizing: border-box;
+  & > .title {
+    font-size: 20px;
+    font-weight: 600;
+    margin-bottom: 20px;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #3a3a3a;
+  }
+
+  &:not(:first-child) {
+    margin-top: 12px;
+  }
+`;
+
+const Section = ({ children, ish2Blind, h2Title, ...rest }) => {
+  return (
+    <StyledSection {...rest}>
+      {ish2Blind ? (
+        <h2 className="blind">{h2Title}</h2>
+      ) : (
+        <h2 className="title">{h2Title}</h2>
+      )}
+      {children}
+    </StyledSection>
+  );
+};
+
+Section.defaultProps = {
+  ish2Blind: false,
+};
+
+export default Section;
