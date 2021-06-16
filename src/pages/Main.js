751d5eed18fffe3c00b41dba87f838d0115dcca9
Ha young, Kim <jeremiah91@naver.com>
2021-06-16 23:35:51 +0900

    feat : Set the default Theme -- dark mode

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Section from "../components/SectionTemplate";
+import Button from "../components/Button";
 
 const Main = () => {
   return (
     <>
-      <div>Main page here</div>
+      <Section h2Title="Buttons">
+        <Button color="warning" outline>
+          warning
+        </Button>
+        <Button color="error">error</Button>
+        <Button color="warning">경고</Button>
+      </Section>
+      <Section ish2Blind h2Title="blind title">
+        hahah
+      </Section>
     </>
   );
 };
